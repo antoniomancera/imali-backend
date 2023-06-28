27ea9e7d37cd4a9f013210594c4b4d4aacfe00ab
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2023-06-28 16:00:03 +0200

    first commit

---
+++ src/main/java/com/antoniomancera/imalibackend/ImaliBackendApplication.java
@@ -0,0 +1,13 @@
+package com.antoniomancera.imalibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaliBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaliBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoniomancera/imalibackend/ImaliBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniomancera.imalibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImaliBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
