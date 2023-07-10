37b20c5d0d96916b7a98cf2601b12e71e1df469e
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2023-07-10 11:09:21 +0200

    feat: addition create and read method for payment

---
+++ src/main/java/com/mancera/imali/models/Payment.java
@@ -0,0 +1,22 @@
+package com.mancera.imali.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "amount")
+    private Integer amount;
+}
---
+++ src/main/java/com/mancera/imali/services/impl/PaymentServiceImpl.java
@@ -0,0 +1,5 @@
+package com.mancera.imali.services.impl;
+
+import com.mancera.imali.services.PaymentService;
+public class PaymentServiceImpl implements PaymentService {
+}
